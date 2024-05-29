b5af330275171bfa78ff8c92c9bf587211e7669e
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2024-05-29 19:56:44 +0530

    initial

---
+++ app/src/main/java/com/shubham/geminiaiapp/Utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.shubham.geminiaiapp.Utils
+
+class Constants {
+    companion object{
+
+        val API_KEY = "AIzaSyCJhGE5myC4Vj3nPHBpIoWPqu80Gv7AkwQ";
+        val MODEL_NAME = "gemini-1.5-flash";
+    }
+}
